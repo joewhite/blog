e80c278a84eb6410fef56795184d138c6c5f7498
Joe White <108666+joewhite@users.noreply.github.com>
2022-09-14 01:44:51 +0000

    Fix #MeToo bug

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 const {withContentlayer} = require('next-contentlayer');
 
-module.exports = withContentlayer({});
+module.exports = withContentlayer({
+  async redirects() {
+    return [
+      {
+        // Oh my. Let's update this for a post-#MeToo consciousness.
+        source: '/2004/09/11/hotel-philosophy-101-through-499-loot-pillage-and-rape',
+        destination: '/2004/09/11/hotel-philosophy-101-through-499-loot-pillage-and-burn',
+        permanent: true,
+      },
+    ];
+  },
+});
